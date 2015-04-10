ac8ec3dfe035834b00e178fa18586f135a3e8956
darksim735 <darksim735@gmail.com>
2015-04-09 21:14:05 -0400

    this works but we need to refactor and add element to home state

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,29 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 			url: '/home',
 			template: '<h1>Hello</h1>'
 		})
+        .state('addcard', {
+			url: '/addcard',
+			templateUrl: 'templates/newcard.html'
+		})
+//        .state('Express', {
+//            url: 'home/express',
+//			templateUrl: 'templates/loadedcard.html'
+//		})
+
+
+        .state('frog', {
+            url: '/:category',
+			templateUrl: 'templates/loadedcard.html'
+		})
+//        .controller('MainController', function($scope){
+//                $scope.categories = [
+//        'MongoDB',
+//        'Express',
+//        'Angular',
+//        'Node'
+//    ];
+//        })
+
+
+
 })
